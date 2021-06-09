61d6258447d1785f31142108186b2cf261fc7a70
Alexander Kalabin <k414bin@yandex.ru>
2021-06-09 12:48:37 +0300

    One test left to fix

---
+++ InvoiceAggregateTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using Timesheets.Models.Entities;
+using System.Linq;
+
+namespace TimesheetsTests
+{
+    public class InvoiceAggregateTests
+    {
+        private static Random rnd = new Random();
+        public static int startIndex = rnd.Next(1, 8);
+        private IEnumerable<int> testRange = Enumerable.Range(1, startIndex);
+
+        public static IEnumerable<Sheet> TestSheets = new List<Sheet>()
+        {
+        };
+    
+        [Fact]
+        public void InvoiceAggregate_CreateRequest()
+        {
+            var builder = new InvoiceAggregateBuilder();
+            var sheet = builder.CreateTestObjectInvoiceAggregate();
+            sheet.ContractID.Should().Be(builder.SheetContractId);
+            sheet.DateStart.Should().BeExactly(TimeSpan.FromSeconds(DateTimeOffset.Now.ToUnixTimeSeconds()));
+            sheet.DateEnd.Should().BeExactly(TimeSpan.FromSeconds(DateTimeOffset.Now.ToUnixTimeSeconds()));
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+
+            yield return new object[]
+            {
+                TestSheets
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void InvoiceAggregate_CalcTheTestSum( IEnumerable<Sheet> testSheets)
+        {
+            var builder = new InvoiceAggregateBuilder();
+            var sheet = builder.CreateTestObjectInvoiceAggregate();
+
+            sheet.IncludeSheets(testSheets);
+            sheet.Should().Be(testSheets);
+        }
+    }
+}
---
+++ SheetAggregateTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace TimesheetsTests
+{
+    public class SheetAggregateTests
+    {
+         public static Guid EmployeeId1 = Guid.Parse("192675bf-2fc7-4e5e-b1b9-1cbc3f23bf32");
+
+        [Fact]
+        public void SheetAggregate_CreateRandomFromSheetRequest()
+        {
+
+            var builder = new SheetAggregateBuilder();
+
+            var sheet =  builder.CreateRandomSheet();
+            sheet.Amount.Should().Be(8);
+            sheet.ContractID.Should().Be(builder.SheetContractId);
+            sheet.ServiceID.Should().Be(builder.SheetServiceId);
+            sheet.EmployeeID.Should().Be(builder.SheetEmployeeId);
+     
+            sheet.Date.Should().BeExactly(TimeSpan.FromSeconds(DateTimeOffset.Now.ToUnixTimeSeconds()));
+        }
+
+        [Fact]
+        public void SheetAggregate_WhenApproved_IsApprovedIsTrue()
+        {
+
+            var builder = new SheetAggregateBuilder();
+
+            var sheet =  builder.CreateRandomSheet();
+             
+            sheet.ApproveSheet();
+            sheet.IsApproved.Should().BeTrue();
+          
+            sheet.ApprovedDate.Should().BeExactly(TimeSpan.FromSeconds(DateTimeOffset.Now.ToUnixTimeSeconds()));
+     
+        }
+
+        public static  IEnumerable<object[]> TestData()
+        {
+
+            yield return new object[] { EmployeeId1 };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void SheetAggregate_Changing_EmployeeId( Guid newEmployeeId)
+        {
+            // arrange
+            var builder = new SheetAggregateBuilder();
+            var sheet =  builder.CreateRandomSheet();
+
+            // act
+            sheet.ChangeEmployee(newEmployeeId);
+
+            // assert
+            sheet.EmployeeID.Should().Be(newEmployeeId);
+        }
+
+    }
+}
